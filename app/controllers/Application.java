196b1cb5da16f7896ed5db6796e1a4342d2dccc5
Zhang Xiawei <zhang_xiawei@hotmail.com>
2014-08-15 11:28:05 +0800

    Full UI Templates and base template for UI

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,18 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+import views.html.strategies;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Really? Your new application is ready."));
+    }
+
+    public static Result strategies() {
+        return ok(strategies.render(""));
     }
 
 }
