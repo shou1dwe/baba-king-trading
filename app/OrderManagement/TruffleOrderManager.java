11465c20a93f98b47534f0f0564376add0b1302e
henrypham <pttrunghieu@gmail.com>
2014-08-19 20:27:10 +0800

    returning submitted trade (with assigned id) after calling async orderSubmit method

--- app/OrderManagement/TruffleOrderManager.java
+++ app/OrderManagement/TruffleOrderManager.java
@@ -30,14 +30,20 @@ public class TruffleOrderManager {
         }
     }
 
+    /**
+     * Submit the trade asynchronously
+     *
+     * @return  the submitted trade
+     */
     public Trade submitTrade(String tickerSymbol, boolean isBuyOrder, int size, double price, TruffleOrderConfirmationListener orderConfirmationListener){
         int id = (int) (System.currentTimeMillis() / 1000L);
         Trade trade = new Trade(id, tickerSymbol, isBuyOrder, size, price);
         listeners.put(id, orderConfirmationListener);
         try {
-            Trade tradeReply = broker.submitTradeAsync(trade);
-            // TODO start a timeout timer
-            return tradeReply;
+            broker.submitTradeAsync(trade);
+            // TODO start a timeout timer for failed case
+            // orderConfirmationListener.onOrderFailed(trade);
+            return trade;
         } catch (WrongTradeFormatException e) {
             return null;
         }
