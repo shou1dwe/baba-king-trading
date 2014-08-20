c130b9918eadc9f2705424662240a43b4df0d427
Zhang Xiawei <zhang_xiawei@hotmail.com>
2014-08-20 10:06:48 +0800

    new post handler for strategy post

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import OrderManagement.TruffleOrderManager;
 import OrderManagement.datatransferobjects.Trade;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import datamanagement.TruffleDataManager;
+import org.apache.activemq.kaha.impl.DataManager;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -15,16 +17,20 @@ import java.util.Map;
 public class Application extends Controller {
 
     private static TruffleOrderManager orderManager = new TruffleOrderManager();
+    private static TruffleDataManager truffleDataManager= new TruffleDataManager();
+
 
     public static Result index() {
         return ok(index.render("Really? Your new application is ready."));
     }
 
     public static Result strategies() {
+
         return ok(strategies.render(""));
     }
 
     public static Result strategyView(long id) {
+
         return ok(strategy_view.render("{Strategy Title}"));
     }
 
@@ -32,8 +38,31 @@ public class Application extends Controller {
         return ok(strategy_create.render("New Strategy"));
     }
 
+    public static Result strategyCreatePost() {
+        Map<String, String[]> params = request().body().asFormUrlEncoded();
+        String tickerSymbol = params.get("ticker")[0];
+        Integer templateId = Integer.parseInt(params.get("templateId")[0]);
+        Integer volume = Integer.parseInt(params.get("volume")[0]);
+        Integer longPeriod = Integer.parseInt(params.get("longDur")[0]);
+        Integer shortPeriod = Integer.parseInt(params.get("shortDur")[0]);
+        Double profitPercent = Double.parseDouble(params.get("profitPer")[0]);
+        Double lossPercent = Double.parseDouble(params.get("lossPer")[0]);
+
+        try {
+            truffleDataManager.insertStrategyNew(tickerSymbol, longPeriod, shortPeriod, volume, lossPercent, profitPercent, templateId);
+            return strategies();
+        } catch (Exception e) {
+            System.err.println(e);
+            return internalServerError();
+        }
+    }
+
     public static Result strategyModify(long id) {
         return ok(strategy_modify.render("Modify {Strategy Title}"));
+    }
+
+    public static Result strategyModifyPost(long id) {
+        return play.mvc.Results.TODO;
     }
 
     public static Result strategyRemove(long id) {
